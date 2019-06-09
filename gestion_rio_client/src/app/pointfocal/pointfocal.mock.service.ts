2f7456f7f6b322a162140ae06acdf12a7da28d9d
wailchalabi <wailchalabi1996@gmail.com>
2019-06-09 20:31:10 +0100

    yet another mistakes

--- gestion_rio_client/src/app/pointfocal/pointfocal.mock.service.ts
+++ gestion_rio_client/src/app/pointfocal/pointfocal.mock.service.ts
@@ -1,12 +1,12 @@
 import {Injectable} from '@angular/core';
 import {Pointfocal} from '../shared/pointfocal';
 
-@Injectable
+
 export class PointFocalMockService{
   private POINTSFOCAUX: Pointfocal[] = [];
   constructor(){
     let pf1:Pointfocal = new Pointfocal(1,'ENSIAS',20,1365);
-    let pf2:Pointfocal = new Pointfocal(2,'ENSET',11,1365);
+    let pf2:Pointfocal = new Pointfocal(2,'ENSET',11,1365.25);
     let pf3:Pointfocal = new Pointfocal(3,'EMI',53,1365);
     this.POINTSFOCAUX.push(pf1);
     this.POINTSFOCAUX.push(pf2);
